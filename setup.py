f8b61830a98c398f92fd5c57487f631158856de3
Thomas Champion <thomas@thomaschampion.net>
2023-03-12 17:18:04 -0600

    first commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Prediction if an individual will default on their credit card',
+    author='Thomas Champion',
+    license='MIT',
+)
